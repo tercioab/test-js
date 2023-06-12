04a4b9d8b18108ff02eb74deded28b95e2fa257f
tercioab <suaagenciainicial@gmail.com>
2023-06-11 22:28:36 -0300

    remove: files

--- task/src/app.module.ts
+++ task/src/app.module.ts
@@ -1,11 +1,8 @@
 import {
   Module,
   NestModule,
-  // RequestMethod,
   MiddlewareConsumer,
 } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
@@ -16,8 +13,6 @@ import { TasksController } from './tasks/tasks.controller';
     MongooseModule.forRoot('mongodb://localhost:27017/teste'),
     TasksModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
